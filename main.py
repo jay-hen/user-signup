93c4fb1f7fb0c61fa1107e0594f5cc08662380f6
Jess Henry <jessjdhenry@gmail.com>
2019-04-16 09:32:32 -0500

    set up templates, form structure

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
 
 app = Flask(__name__)
 app.config['debug'] = True
 
-form = """
-
-"""
-
 @app.route("/")
 def index():
-    return form
+    return render_template('form.html')
 
-app.run
+if __name__ == '__main__':
+    app.run
